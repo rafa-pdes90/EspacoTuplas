c749ac4b79bcc23d9b35119c305142de85a4cf76
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-05-22 15:50:44 -0300

    Index alterado para time

--- project/space/Mensagem.java
+++ project/space/Mensagem.java
@@ -1,7 +1,7 @@
 import net.jini.core.entry.Entry;
 
 public class Mensagem implements Entry {
-    public Integer index;
+    public Long time;
     public String from;
     public String to;
     public String msg;
